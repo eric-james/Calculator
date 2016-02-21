aeb770a9a76153a86490f981a1db5ef64cb15049
Eric Bryan <phoenixforce44@gmail.com>
2016-02-21 15:28:24 -0700

    Initial check in of calculator program.

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,81 @@
+
+package cse360assign3;
+
+/**
+ * Class to perform basic calculator functions such as adding, subtracting, multiplying, etc. 
+ * 
+ * @author Eric-James Bryan
+ * PIN: 202
+ * 
+ */
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the current running total for the calculator's calculations.
+	 * 
+	 * @param none
+	 * @return an integer total
+	 */
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Takes in an integer and adds it to the calculations list and the total
+	 * 
+	 * @param new Integer to be added
+	 */
+	
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Takes in an integer and subtracts it from the calculations list and the total
+	 * 
+	 * @param new Integer to be subtracted
+	 */
+	
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Takes in an integer and multiplies the current total by this integer.
+	 * 
+	 * @param new Integer to be multiplied
+	 */
+	
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Takes in an integer and divides the current total by this integer.
+	 * 
+	 * @param new Integer to be divided
+	 */
+	
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Returns the total list of calculations that have been performed by the calculator method
+	 * 
+	 * @param none
+	 * @return string of calculations
+	 */
+	
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,78 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void Addtest1() {
+		Calculator calc = new Calculator();
+		calc.add(5);
+		calc.add(7);
+		int total= calc.getTotal();
+		assertEquals(total, 12);
+	}
+	@Test
+	public void Addtest2() {
+		Calculator calc = new Calculator();
+		calc.add(5);
+		calc.add(7);
+		calc.add(-2);
+		int total= calc.getTotal();
+		assertEquals(total, 10);
+	}	
+	@Test
+	public void subtractTest1() 
+	{
+		Calculator calc = new Calculator();
+		calc.subtract(14);
+		calc.subtract(6);
+		int total= calc.getTotal();
+		assertEquals(total, 20);
+	}
+	@Test
+	public void subtractTest2() 
+	{
+		Calculator calc = new Calculator();
+		calc.subtract(14);
+		calc.subtract(6);
+		calc.subtract(-5);
+		int total= calc.getTotal();
+		assertEquals(total, 15);
+	}
+	@Test
+	public void multiplyTest1() 
+	{
+		Calculator calc = new Calculator();
+		calc.multiply(3);
+		int total= calc.getTotal();
+		assertEquals(total, 0);
+	}
+	@Test
+	public void multiplyTest2() 
+	{
+		Calculator calc = new Calculator();
+		calc.add(7);
+		calc.multiply(3);
+		int total= calc.getTotal();
+		assertEquals(total, 21);
+	}
+	@Test
+	public void divideTest() 
+	{
+		Calculator calc = new Calculator();
+		calc.add(15);
+		calc.divide(5);
+		int total= calc.getTotal();
+		assertEquals(total, 3);
+	}
+	@Test
+	public void calcTest() 
+	{
+		Calculator calc = new Calculator();
+		int total= calc.getTotal();
+		assertEquals(total, 0);
+	}
+}
